5341aec3e1fba3951f1ef9f929d7b9788fcc22be
AlexanderDerevliov <alex.feelred@gmail.com>
2024-01-15 11:22:07 +0100

    tried to fixed, still working on it

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+@extends('layout.app')
+
+@section('content')
+<!-- TODO modifica sintassi immagini per build -->
+<section id="jumbotron">
+
+</section>
+<!-- /#jumbotron -->
+
+
+<section id="current_series">
+
+    <div class="container">
+
+        <span class="section_comics blue_ticket">
+            CURRENT SERIES
+        </span>
+
+        <div class="row px-2 py-3 justify-content-center">
+
+
+
+            <!-- CARDS -->
+            @include('layout.partials.card')
+
+
+
+        </div>
+
+        <div class="d-flex justify-content-center">
+            <span class="blue_ticket text-center mb-3">
+                <a class="text-decoration-none text-white" href="##">LOAD MORE</a>
+            </span>
+        </div>
+    </div>
+</section>
+<!-- /#current_series -->
+@endsection
